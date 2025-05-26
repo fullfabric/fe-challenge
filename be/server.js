2f6cfc15a9dd8cd26062d07e42ad3e78bcc14790
João Ferreira <joao.ferreira@fullfabric.com>
2025-05-26 20:25:02 +0100

    Add CORS to API

--- be/server.js
+++ be/server.js
@@ -1,5 +1,7 @@
 require('./models')
 
+const cors = require('@koa/cors')
+
 const app = require('./')
 const sequelize = require('./sequelize')
 
@@ -11,4 +13,15 @@ module.exports = (async () => {
   app.listen(8080, () => {
     console.log('Serving API on http://localhost:8080')
   })
+
+  const codespaceName = process.env.CODESPACE_NAME
+
+  if (codespaceName) {
+    const portForwardingDomain = process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN
+    const origin = `https://${codespaceName}-8080.${portForwardingDomain}`
+
+    app.use(cors({ origin }))
+
+    console.log(`CORS enabled for ${origin}`)
+  }
 })()
